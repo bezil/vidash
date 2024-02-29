63b8ea96153db692fb15bd2fe3aebe87fdbd42d6
Basil <basil.george@aspireapp.com>
2024-02-29 18:58:16 +0530

    env

--- vidash-web-live/src/lib/liveblocks.ts
+++ vidash-web-live/src/lib/liveblocks.ts
@@ -3,7 +3,7 @@ import { createClient,
 } from "@liveblocks/client";
 
 export const client = createClient({
-  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY ?? '',
   throttle: 16,
 });
 
