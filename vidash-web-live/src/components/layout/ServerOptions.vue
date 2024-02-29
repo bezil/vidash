b713a184b97f19fe9f4a8c0e5ba4b6f957613dbf
Basil <basil.george@aspireapp.com>
2024-02-29 18:21:34 +0530

    fix env

--- vidash-web-live/src/components/layout/ServerOptions.vue
+++ vidash-web-live/src/components/layout/ServerOptions.vue
@@ -2,7 +2,7 @@
 import { Server } from '@/types/common.ts'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { MonitorDot, ChevronsUpDown, Trash, Lock,
-  Users, Hammer, TableProperties, Bot, Radio,
+  Users, Hammer, TableProperties, Bot, Radio, UserRoundPlus,
 } from 'lucide-vue-next'
 import {
   DropdownMenu,
@@ -38,11 +38,15 @@ const deleteServer = () => {
                 <ChevronsUpDown  class="w-4 h-4" />
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-            <DropdownMenuLabel class="font-light text-sm">Dash Options</DropdownMenuLabel>
+            <DropdownMenuLabel class="font-light text-xs">Dash Options</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-                <Trash class="mr-2 h-4 w-4" />
-                <span @click="deleteServer">Delete Dash</span>
+                <UserRoundPlus class="mr-2 h-3 w-3" />
+                <span class="text-sm">Invite Member</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <Trash class="mr-2 h-3 w-3" />
+                <span class="text-sm" @click="deleteServer">Delete Dash</span>
             </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
@@ -51,19 +55,19 @@ const deleteServer = () => {
       <ScrollArea class="flex-1">
         <div class="flex flex-col space-between">
           <div class="flex w-full justify-between items-center px-2 pt-4">
-            <p class="uppercase text-ns items-center"><Users class="w-3 h-3 mr-1 inline text-ring" /> MEMBERS</p>
+            <p class="uppercase text-ns items-center pb-1"><Users class="w-3 h-3 mr-1 inline text-ring" /> MEMBERS</p>
             <Lock class="w-3 h-3 mr-1  text-red-500"/>
           </div>
           <div class="flex w-full justify-between items-center px-2 pt-4">
-            <p class="uppercase text-ns items-center"><TableProperties class="w-3 h-3 mr-1 inline text-ring" /> CHANNELS</p>
+            <p class="uppercase text-ns items-center pb-1"><TableProperties class="w-3 h-3 mr-1 inline text-ring" /> CHANNELS</p>
             <Lock class="w-3 h-3 mr-1  text-red-500"/>
           </div>
           <div class="flex flex-col w-full items-left text-left px-2 pt-4">
-            <p class="uppercase text-ns items-center"><Hammer class="w-3 h-3 mr-1 inline text-ring" /> TOOLS</p>
-            <p class="text-sm mt-1 px-1 flex justify-between items-center cursor-pointer hover:bg-secondary rounded-sm">Live
+            <p class="uppercase text-ns items-center pb-1"><Hammer class="w-3 h-3 mr-1 inline text-ring" /> TOOLS</p>
+            <p class="text-sm py-1 pl-5 pr-1 flex justify-between items-center cursor-pointer hover:bg-secondary rounded-sm">Live
               <Radio class="w-4 h4 inline text-ring" />
             </p>
-            <p class="text-sm mt-1 px-1 flex justify-between items-center cursor-pointer hover:bg-secondary rounded-sm">AI
+            <p class="text-sm py-1 pl-5 pr-1 flex justify-between items-center cursor-pointer hover:bg-secondary rounded-sm">AI
               <Bot class="w-4 h-4 inline text-ring" />
             </p>
           </div>
